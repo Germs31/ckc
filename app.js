f0ed0cfbfbc30951fa531e18da6cf2fe19d655b2
German Nunez <german90nunez@gmail.com>
2020-02-01 10:35:03 -0800

    update

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-// console.log('hello')
-
-// for(i = 0; i <= 100; i++){
-//     if(i % 3 == 0 && i % 5 == 0)
-//     {
-//         console.log('fizbuzz');
-//     } else if (i % 3 == 0)
-//     {
-//         console.log('fizz')
-//     } else if(i % 5 == 0){
-//         console.log('buzz')
-//     } else {
-//         console.log(i)
-//     }
-    
-//}
-
-
-//write a javascript function this function will take two perams 
-// 1 is a var target # 
-// 2 array of int
-// goal is to look through 
-// 5 [0,1,3,2,6]
-
-// x = (tar, arr) => {
-//     for(i = 0; i < arr.length; i++){
-//         for(j = i+1; j < arr.length; j++){
-//             if(arr[i] + arr[j] == tar){
-//                 let y = [];
-//                 y.push(arr[i], arr[j])
-//                 // console.log(y);
-//                 return y
-//             }
-//             // console.log(arr[j])
-//         }
-//         // console.log(arr[i])
-//     }
-// }
-
-// console.log(x(5, [0,1,3,2,6]));
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
